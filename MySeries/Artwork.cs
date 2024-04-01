58b7f2fabc00156f32009813596021d8417a76e1
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-04-01 02:53:41 -0400

    Added the Artwork and Repository classes!

---
+++ MySeries/Artwork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MySeries.Classes
+{
+    public class Artwork
+    {
+        protected string title;
+        protected string author;
+        protected string date;
+        protected int rating;
+        protected string rewiew;
+
+        public Artwork(string title, string author, string date, int rating, string rewiew)
+        {
+            this.title = title;
+            this.author = author;
+            this.date = date;
+            this.rating = rating;
+            this.rewiew = rewiew;
+        }
+
+        float getStars()
+        {
+            return (float)rating * 0.5f;
+        }
+
+        void copy(Artwork original)
+        {
+            this.title = original.title;
+            this.author = original.author;
+            this.date = original.date;
+            this.rating = original.rating;
+            this.rewiew = original.rewiew;
+        }
+
+        void modify(string title, string author, string date, int rating, string rewiew)
+        {
+            this.title = title;
+            this.author = author;
+            this.date = date;
+            this.rating = rating;
+            this.rewiew = rewiew;
+        }
+    }
+}
---
+++ MySeries/Repository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace MySeries.Classes
+{
+    public class Repository<T>
+    {
+        private List<T> list;
+
+        public Repository() 
+        {
+            list = new List<T>();
+        }
+
+        public Repository(List<T> list)
+        {
+            this.list = list;
+        }
+
+        public void add(T element)
+        {
+            list.Add(element);
+        }
+
+        public void eliminate(T element)
+        {
+            if (list.Contains(element))
+            {
+                list.Remove(element);
+            }
+        }
+
+        public ReadOnlyCollection<T> getList()
+        {
+            return list.AsReadOnly();            
+        }
+    }
+}
