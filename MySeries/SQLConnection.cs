05097b355169a31105c58c59553796f5583aedcf
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-05-15 14:01:59 -0400

    Added SQL-executing functions

--- MySeries/SQLConnection.cs
+++ MySeries/SQLConnection.cs
@@ -27,5 +27,27 @@ namespace SQL
             connection.Close();
             connection = null;
         }
+
+        public static int ExecuteNonQuery(string sqlStatement) 
+        {
+            using (SqlCommand command = new SqlCommand(sqlStatement, connection))
+            {
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        public static object ExecuteScalar(string sqlStatement)
+        {
+            using (SqlCommand command = new SqlCommand(sqlStatement, connection))
+            {
+                return command.ExecuteScalar();
+            }
+        }
+
+        public static SqlDataReader ExecuteReader(string sqlStatement)
+        {
+            SqlCommand command = new SqlCommand(sqlStatement, connection);
+            return command.ExecuteReader();
+        }
     }
 }
